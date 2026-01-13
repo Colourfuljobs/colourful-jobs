c8f570714218773ca55f5d9f1f4b8a0a6454d130
Colourfuljobs <accounts@colourfuljobs.nl>
2026-01-13 16:34:39 +0100

    Fix: add basePath to SessionProvider to prevent wrong POST requests

--- src/components/SessionProvider.tsx
+++ src/components/SessionProvider.tsx
@@ -4,7 +4,11 @@ import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
 import type { ReactNode } from "react";
 
 export function SessionProvider({ children }: { children: ReactNode }) {
-  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
+  return (
+    <NextAuthSessionProvider basePath="/api/auth" refetchOnWindowFocus={false}>
+      {children}
+    </NextAuthSessionProvider>
+  );
 }
 
 
